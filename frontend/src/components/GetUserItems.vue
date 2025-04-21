fd5c1d0983a0e7ab757eb06eb9cdebdce5dc9568
elmar-guevarra <elmar.guevarra@iress.com>
2025-04-21 13:46:45 +0800

    Arrived badge

--- frontend/src/components/GetUserItems.vue
+++ frontend/src/components/GetUserItems.vue
@@ -8,11 +8,11 @@
     <h5 v-if="todayVisitors.length > 0" class="mt-4">Today's Visitors</h5>
     <div v-for="visitor in todayVisitors" :key="visitor.registrationId" class="card mb-3">
       <div class="card-body d-flex flex-column align-items-center">
-        <span 
-            v-if="visitor.hasArrived" 
-            class="badge bg-success bg-opacity-50 fw-normal"
-            style="font-size: 0.75rem; position: absolute; top: 10px; right: 10px;"
-          >
+        <span
+          v-if="visitor.hasArrived"
+          class="alert alert-success py-1 px-2 fw-normal position-absolute top-0 end-0 mt-2 me-2"
+          style="font-size: 0.75rem; border-radius: 0.2rem;"
+        >
           Arrived
         </span>
         <h6 class="card-title text-center mb-2">
