67c4256b34628d37ca16ba2977d986cb9a1d0868
elmar-guevarra <elmar.guevarra@iress.com>
2025-04-30 22:27:10 +0800

    format date utils

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,4 +1,4 @@
-export function getYearMonthDay(date: Date | string | number): string {
+export function getYearMonthDay(date: Date): string {
   let dateObject: Date
   if (date instanceof Date) {
     dateObject = date
@@ -14,4 +14,13 @@ export function getYearMonthDay(date: Date | string | number): string {
   const month = (dateObject.getMonth() + 1).toString().padStart(2, '0')
   const day = dateObject.getDate().toString().padStart(2, '0')
   return `${year}-${month}-${day}`
+}
+
+export function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  return date.toLocaleDateString(undefined, options)
 }
