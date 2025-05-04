52e441bea79c713eecd89d689d590ec4926e74da
elmar-guevarra <elmar.guevarra@iress.com>
2025-05-04 21:48:04 +0800

    rollback authentication

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -72,34 +72,34 @@ const router = createRouter({
   routes,
 })
 
-router.beforeEach(async (to, from, next) => {
-  if (to.meta.requiresAuth) {
-    try {
-      const oauth = {
-        domain: 'ap-southeast-1scrgvy8xg.auth.ap-southeast-1.amazoncognito.com',
-        scope: [
-          'openid',
-          'email',
-          'phone',
-          // 'profile',
-          'aws.cognito.signin.user.admin',
-        ].join(' '), // Join scopes with a space
-        redirectSignIn: window.location.origin + to.fullPath,
-        responseType: 'code',
-        clientId: '3jid0987p3l05ursbrs6gun0oi',
-      }
+// router.beforeEach(async (to, from, next) => {
+//   if (to.meta.requiresAuth) {
+//     try {
+//       const oauth = {
+//         domain: 'ap-southeast-1scrgvy8xg.auth.ap-southeast-1.amazoncognito.com',
+//         scope: [
+//           'openid',
+//           'email',
+//           'phone',
+//           // 'profile',
+//           'aws.cognito.signin.user.admin',
+//         ].join(' '), // Join scopes with a space
+//         redirectSignIn: window.location.origin + to.fullPath,
+//         responseType: 'code',
+//         clientId: '3jid0987p3l05ursbrs6gun0oi',
+//       }
 
-      // const cognitoUrl = `https://${oauth.domain}/login?client_id=${oauth.clientId}&response_type=${oauth.responseType}&scope=${encodeURIComponent(oauth.scope)}&redirect_uri=${encodeURIComponent(oauth.redirectSignIn)}`
-      const cognitoUrl = `https://ap-southeast-1scrgvy8xg.auth.ap-southeast-1.amazoncognito.com/login/continue?client_id=3jid0987p3l05ursbrs6gun0oi&redirect_uri=https%3A%2F%2Fvms.alphinecodetech.click&response_type=code&scope=email+openid`
-      console.log('cognitoUrl: ', cognitoUrl)
-      window.location.href = cognitoUrl
-    } catch (error) {
-      console.error('Authentication error:', error)
-    }
-  } else {
-    next() // Route does not require authentication
-  }
-})
+//       // const cognitoUrl = `https://${oauth.domain}/login?client_id=${oauth.clientId}&response_type=${oauth.responseType}&scope=${encodeURIComponent(oauth.scope)}&redirect_uri=${encodeURIComponent(oauth.redirectSignIn)}`
+//       const cognitoUrl = `https://ap-southeast-1scrgvy8xg.auth.ap-southeast-1.amazoncognito.com/login/continue?client_id=3jid0987p3l05ursbrs6gun0oi&redirect_uri=https%3A%2F%2Fvms.alphinecodetech.click&response_type=code&scope=email+openid`
+//       console.log('cognitoUrl: ', cognitoUrl)
+//       window.location.href = cognitoUrl
+//     } catch (error) {
+//       console.error('Authentication error:', error)
+//     }
+//   } else {
+//     next() // Route does not require authentication
+//   }
+// })
 
 const app = createApp(App)
 app.use(router)
