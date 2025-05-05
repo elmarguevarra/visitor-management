a53be18ae834623957ccf96da34a4876cdc280a5
elmar-guevarra <elmar.guevarra@iress.com>
2025-05-05 16:08:08 +0800

    removed wrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,119 +20,115 @@ if (process.env.NODE_ENV === 'development') {
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
-async function initApp() {
-  let user = await userManager.getUser()
+let user = await userManager.getUser()
 
-  const pinia = createPinia()
-  const routes = [
-    {
-      path: '/',
-      name: 'ListVisitors',
-      component: ListVisitorsView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/register-visitor',
-      name: 'Register',
-      component: RegisterVisitorView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/get-visitor',
-      name: 'GetVisitor',
-      component: GetVisitorView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/invite-visitor',
-      name: 'InviteVisitorView',
-      component: InviteVisitorView,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/self-register-visitor/:inviteToken',
-      name: 'SelfRegisterVisitorView',
-      component: SelfRegisterVisitorView,
-      props: true,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/verify-visitor/:registrationId',
-      name: 'VerifyVisitorView',
-      component: VerifyVisitorView,
-      props: true,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      redirect: '/',
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/signin-callback',
-      name: 'SignInCallback',
-      component: {
-        template: '<div>Processing login...</div>',
-        async created() {
-          const router = useRouter()
-          try {
-            await userManager.signinRedirectCallback()
-            router.push('/')
-          } catch (error) {
-            console.error('Error handling sign-in callback:', error)
-            router.push('/login-error')
-          }
-        },
-      },
-      meta: { requiresAuth: false },
-    },
-    {
-      path: '/signout-callback',
-      name: 'SignOutCallback',
-      component: {
-        template: '<div>Processing logout...</div>',
-        async created() {
-          user = null
-          try {
-            await userManager.signoutRedirectCallback()
-            router.push('/')
-          } catch (error) {
-            console.error('Error handling sign-out callback:', error)
-            router.push('/')
-          }
-        },
-      },
-      meta: { requiresAuth: false },
-    },
-  ]
-
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  })
-
-  router.beforeEach(async (to, from, next) => {
-    if (to.meta.requiresAuth) {
-      if (user) {
-        next()
-      } else {
-        // Redirect to Cognito for login, but only if not already on the callback page
-        if (to.name !== 'SignInCallback') {
-          await userManager.signinRedirect()
-        } else {
-          next()
+const pinia = createPinia()
+const routes = [
+  {
+    path: '/',
+    name: 'ListVisitors',
+    component: ListVisitorsView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/register-visitor',
+    name: 'Register',
+    component: RegisterVisitorView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/get-visitor',
+    name: 'GetVisitor',
+    component: GetVisitorView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/invite-visitor',
+    name: 'InviteVisitorView',
+    component: InviteVisitorView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/self-register-visitor/:inviteToken',
+    name: 'SelfRegisterVisitorView',
+    component: SelfRegisterVisitorView,
+    props: true,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/verify-visitor/:registrationId',
+    name: 'VerifyVisitorView',
+    component: VerifyVisitorView,
+    props: true,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    redirect: '/',
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/signin-callback',
+    name: 'SignInCallback',
+    component: {
+      template: '<div>Processing login...</div>',
+      async created() {
+        const router = useRouter()
+        try {
+          await userManager.signinRedirectCallback()
+          router.push('/')
+        } catch (error) {
+          console.error('Error handling sign-in callback:', error)
+          router.push('/login-error')
         }
-      }
-    } else {
+      },
+    },
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/signout-callback',
+    name: 'SignOutCallback',
+    component: {
+      template: '<div>Processing logout...</div>',
+      async created() {
+        user = null
+        try {
+          await userManager.signoutRedirectCallback()
+          router.push('/')
+        } catch (error) {
+          console.error('Error handling sign-out callback:', error)
+          router.push('/')
+        }
+      },
+    },
+    meta: { requiresAuth: false },
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (user) {
       next()
+    } else {
+      // Redirect to Cognito for login, but only if not already on the callback page
+      if (to.name !== 'SignInCallback') {
+        await userManager.signinRedirect()
+      } else {
+        next()
+      }
     }
-  })
+  } else {
+    next()
+  }
+})
 
-  const app = createApp(App)
-  app.use(router)
-  app.use(pinia)
-  app.mount('#app')
-}
-
-initApp()
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
