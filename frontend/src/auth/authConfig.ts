bb3b403984f36a4d1f11743920b42a4b53801189
Elmar Guevarra <64261171+elmarguevarra@users.noreply.github.com>
2025-05-10 02:35:14 +0000

    cognitoAuthConfig

--- frontend/src/auth/authConfig.ts
+++ frontend/src/auth/authConfig.ts
@@ -1,11 +1,12 @@
 import { UserManager } from 'oidc-client-ts'
 
-const USER_POOL_ID = process.env.COGNITO_USER_POOL_ID || ''
-const USER_POOL_CLIENT_ID = process.env.COGNITO_USER_POOL_CLIENT_ID || ''
+// const USER_POOL_CLIENT_ID = process.env.COGNITO_USER_POOL_CLIENT_ID || ''
 // const USER_POOL_DOMAIN_URL = process.env.COGNITO_USER_POOL_DOMAIN_URL || ''
-const USER_POOL_DOMAIN_URL = 'https://alphine-vms-auth.auth.ap-southeast-1.amazoncognito.com' //TODO: Use env variable
-const AUTHORITY_URL = process.env.COGNITO_AUTHORITY_URL || ''
+// const AUTHORITY_URL = process.env.COGNITO_AUTHORITY_URL || ''
 
+const USER_POOL_CLIENT_ID = '32kjaah9rh2c0q38dghktm71t1'
+const USER_POOL_DOMAIN_URL = 'https://alphine-vms-auth.auth.ap-southeast-1.amazoncognito.com'
+const AUTHORITY_URL = 'https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_DSpKRdggr'
 
 const cognitoAuthConfig = {
   authority: AUTHORITY_URL,
