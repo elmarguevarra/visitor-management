4e70fc22f77d30dc3cc359986829ded5099d9df4
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-05-11 17:00:21 +0800

    console

--- frontend/src/auth/authConfig.ts
+++ frontend/src/auth/authConfig.ts
@@ -4,9 +4,24 @@ import { UserManager } from 'oidc-client-ts'
 // const USER_POOL_DOMAIN_URL = process.env.COGNITO_USER_POOL_DOMAIN_URL || ''
 // const AUTHORITY_URL = process.env.COGNITO_AUTHORITY_URL || ''
 
+console.log(
+  'process.env.COGNITO_USER_POOL_CLIENT_ID: ',
+  process.env.COGNITO_USER_POOL_CLIENT_ID,
+)
+console.log(
+  'process.env.COGNITO_USER_POOL_DOMAIN_URL: ',
+  process.env.COGNITO_USER_POOL_DOMAIN_URL,
+)
+console.log(
+  'process.env.COGNITO_AUTHORITY_URL: ',
+  process.env.COGNITO_AUTHORITY_URL,
+)
+
 const USER_POOL_CLIENT_ID = '32kjaah9rh2c0q38dghktm71t1'
-const USER_POOL_DOMAIN_URL = 'https://alphine-vms-auth.auth.ap-southeast-1.amazoncognito.com'
-const AUTHORITY_URL = 'https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_DSpKRdggr'
+const USER_POOL_DOMAIN_URL =
+  'https://alphine-vms-auth.auth.ap-southeast-1.amazoncognito.com'
+const AUTHORITY_URL =
+  'https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_DSpKRdggr'
 
 const cognitoAuthConfig = {
   authority: AUTHORITY_URL,
