2aa76cc1409cdfa4d231bd00a814c635bf6b7ecb
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-05-13 21:33:41 +0800

    fix

--- frontend/src/services/avpApiServices.ts
+++ frontend/src/services/avpApiServices.ts
@@ -5,34 +5,18 @@ import {
   BatchIsAuthorizedInput,
 } from '@aws-sdk/client-verifiedpermissions'
 
-import { EntityIdentifier, Decision } from 'aws-sdk/clients/verifiedpermissions'
-
 const region = process.env.AWS_REGION
 const policyStoreId = process.env.POLICY_STORE_ID
 
 const verifiedPermissionsClient = new VerifiedPermissionsClient({ region })
 
-interface BatchIsAuthorizedResult {
-  decision: Decision
-}
-
 export async function authorizeBatch(
   principalId: string,
   actions: string[],
-): Promise<BatchIsAuthorizedResult[]> {
+): Promise<BatchIsAuthorizedCommandOutput> {
   if (!policyStoreId) {
     console.error('POLICY_STORE_ID environment variable not set!')
-    return []
-  }
-
-  const principalIdentifier: EntityIdentifier = {
-    entityType: 'VisitorManagement::UserGroup',
-    entityId: principalId,
-  }
-
-  const resourceIdentifier: EntityIdentifier = {
-    entityType: 'VisitorManagement::Visitor',
-    entityId: 'visitorResource',
+    return { $metadata: {}, results: [] }
   }
 
   const batchIsAuthorizedInput: BatchIsAuthorizedInput = {
@@ -59,13 +43,9 @@ export async function authorizeBatch(
     const batchIsAuthorizedCommandOutput: BatchIsAuthorizedCommandOutput =
       await verifiedPermissionsClient.send(command)
 
-    return (
-      batchIsAuthorizedCommandOutput.results?.map((result) => ({
-        decision: result.decision as Decision,
-      })) ?? []
-    )
+    return batchIsAuthorizedCommandOutput
   } catch (err: any) {
     console.error('Error checking batch authorization:', err)
-    return []
+    return { $metadata: {}, results: [] }
   }
 }
