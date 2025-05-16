54ce8877ee9b09be817ae2ab2b8d703580ac3cc3
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-05-16 10:50:57 +0800

    restricted page

--- frontend/src/views/RestrictedView.vue
+++ frontend/src/views/RestrictedView.vue
@@ -1,20 +1,20 @@
 <template>
-  <div
-    class="d-flex flex-column justify-content-center align-items-center vh-100 text-center bg-light px-3"
-  >
-    <h1 class="display-4 text-danger mb-3">Access Denied</h1>
-    <p class="lead mb-4">You do not have permission to view this page.</p>
-    <p v-if="justSignedUp" class="mb-3">
-      Thank you for signing up! Your account is awaiting administrator approval.
-      Please be patient while your access is reviewed.
-    </p>
-    <p>
-      If you believe this is an error or your account should already be active,
-      please contact your administrator or
-      <button @click="goHome" class="btn btn-primary btn-sm ms-2">
-        Return to Home
-      </button>
-    </p>
+  <div class="container mt-2">
+    <h4 class="mb-3 text-danger" style="margin-left: -0.2rem">Access Denied</h4>
+    <div class="alert alert-danger" role="alert">
+      <p class="lead mb-2">You do not have permission to view this page.</p>
+      <p class="mb-2">
+        Thank you for signing up! Your account is awaiting administrator
+        approval. Please be patient while your access is reviewed.
+      </p>
+      <p>
+        If you believe this is an error or your access should be different,
+        please contact your administrator or
+        <button @click="goHome" class="btn btn-primary btn-sm ms-2">
+          Return to Home
+        </button>
+      </p>
+    </div>
   </div>
 </template>
 
