3760cdcc456c027bc0d47587fa2b013ad19aa6e7
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-05-16 11:01:20 +0800

    restricted

--- frontend/src/views/RestrictedView.vue
+++ frontend/src/views/RestrictedView.vue
@@ -3,14 +3,10 @@
     <h4 class="mb-3 text-danger" style="margin-left: -0.2rem">Access Denied</h4>
     <div class="alert alert-danger" role="alert">
       <p class="lead mb-2">You do not have permission to view this page.</p>
-      <p class="mb-2">
-        Thank you for signing up! Your account is awaiting administrator
-        approval. Please be patient while your access is reviewed.
-      </p>
       <p>
         If you believe this is an error or your access should be different,
-        please contact your administrator or
-        <button @click="goHome" class="btn btn-primary btn-sm ms-2">
+        please contact your administrator.
+        <button @click="goHome" class="btn btn-primary btn-sm ms-2 mt-2">
           Return to Home
         </button>
       </p>
