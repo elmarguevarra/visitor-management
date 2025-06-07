7380dd3951409a664a81fef376dd0459a5799288
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-06-07 16:45:08 +0800

    const authenticationStore = useAuthenticationStore() const id_token = authenticationStore.user?.id_token

--- frontend/src/services/internalApiServices.ts
+++ frontend/src/services/internalApiServices.ts
@@ -2,8 +2,6 @@ import { useAuthenticationStore } from '@/stores/authenticationStore'
 import axios from 'axios'
 
 const API_BASE = process.env.VUE_APP_API_ENDPOINT || ''
-const authenticationStore = useAuthenticationStore()
-const id_token = authenticationStore.user?.id_token
 
 export async function postVisitor(data: any): Promise<any> {
   const response = await axios.post(`${API_BASE}visitor`, data)
@@ -23,6 +21,9 @@ export async function postVisitRequest(data: any): Promise<any> {
 export async function getVisitorsByResidentId(
   residentId: string,
 ): Promise<any> {
+  const authenticationStore = useAuthenticationStore()
+  const id_token = authenticationStore.user?.id_token
+
   const response = await axios.get(
     `${API_BASE}visitors?residentId=${residentId}`,
     {
