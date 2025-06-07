e48dcde2ae00a9f5cf46fa545bef598a9c34cd69
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-06-07 12:37:30 +0800

    Authorization: `Bearer ${token}`,

--- frontend/src/services/awsServices.ts
+++ frontend/src/services/awsServices.ts
@@ -1,3 +1,4 @@
+import { useAuthenticationStore } from '@/stores/authenticationStore'
 import axios from 'axios'
 
 const API_BASE = process.env.VUE_APP_API_ENDPOINT || ''
@@ -8,8 +9,10 @@ export async function evaluatePermissions(
   userGroup: string | undefined,
   actions: string[] | undefined,
 ): Promise<any> {
-  const params = new URLSearchParams()
+  const authenticationStore = useAuthenticationStore()
+  const token = authenticationStore.user?.access_token
 
+  const params = new URLSearchParams()
   params.append('principalId', userGroup ?? defaultUserGroup)
 
   if (actions && actions.length > 0) {
@@ -21,6 +24,11 @@ export async function evaluatePermissions(
     ? `${API_BASE}permissions?${queryString}`
     : `${API_BASE}permissions`
 
-  const response = await axios.get(url)
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
   return response.data
 }
