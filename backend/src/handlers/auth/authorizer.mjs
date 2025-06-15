bee7b3a5b5da2a9c64bdd3f01a90498e5a427a82
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-06-15 17:25:56 +0800

    test--return true always

--- backend/src/handlers/auth/authorizer.mjs
+++ backend/src/handlers/auth/authorizer.mjs
@@ -20,44 +20,44 @@ const CORS_HEADERS = {
 
 export const verifiedPermissionsAuthorizerHandler = async (event) => {
   try {
-    const token = event.headers.authorization || event.headers.Authorization;
-    if (!token) {
-      return {
-        isAuthorized: false,
-        context: CORS_HEADERS,
-      };
-    }
+    // const token = event.headers.authorization || event.headers.Authorization;
+    // if (!token) {
+    //   return {
+    //     isAuthorized: false,
+    //     context: CORS_HEADERS,
+    //   };
+    // }
 
-    const decoded = await verifyToken(token);
-    const principalId = decoded["cognito:groups"].find(
-      (g) => !g.endsWith("_Google")
-    );
+    // const decoded = await verifyToken(token);
+    // const principalId = decoded["cognito:groups"].find(
+    //   (g) => !g.endsWith("_Google")
+    // );
 
-    const actionId = event.headers["X-Required-Permission"];
+    // const actionId = event.headers["X-Required-Permission"];
 
-    const command = new IsAuthorizedCommand({
-      policyStoreId: policyStoreId,
-      principal: {
-        entityId: principalId,
-        entityType: ENTITY_TYPES.USER_GROUP,
-      },
-      resource: {
-        entityId: RESOURCES.VISITOR,
-        entityType: ENTITY_TYPES.RESOURCE,
-      },
-      action: {
-        actionId: actionId,
-        actionType: ENTITY_TYPES.ACTION,
-      },
-    });
+    // const command = new IsAuthorizedCommand({
+    //   policyStoreId: policyStoreId,
+    //   principal: {
+    //     entityId: principalId,
+    //     entityType: ENTITY_TYPES.USER_GROUP,
+    //   },
+    //   resource: {
+    //     entityId: RESOURCES.VISITOR,
+    //     entityType: ENTITY_TYPES.RESOURCE,
+    //   },
+    //   action: {
+    //     actionId: actionId,
+    //     actionType: ENTITY_TYPES.ACTION,
+    //   },
+    // });
 
-    const authResult = await client.send(command);
-    if (authResult.decision !== "ALLOW") {
-      return {
-        isAuthorized: false,
-        context: CORS_HEADERS,
-      };
-    }
+    // const authResult = await client.send(command);
+    // if (authResult.decision !== "ALLOW") {
+    //   return {
+    //     isAuthorized: false,
+    //     context: CORS_HEADERS,
+    //   };
+    // }
 
     return {
       isAuthorized: true,
