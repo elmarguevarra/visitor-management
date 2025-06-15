c5a5ee0da77ab0c9772685f9dae447e8227fcffb
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-06-15 17:44:13 +0800

    fix--get-visitors-cors headers

--- backend/src/handlers/get-visitors.mjs
+++ backend/src/handlers/get-visitors.mjs
@@ -70,9 +70,11 @@ export const getVisitorsHandler = async (event) => {
   const response = {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers":
+        "Content-Type,Authorization,X-Required-Permission",
       "Access-Control-Allow-Origin": frontEndBaseUrl,
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
+      "Access-Control-Expose-Headers": "X-Required-Permission",
     },
     body: JSON.stringify(items),
   };
