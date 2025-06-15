5dc6272509045810a1ddf4522610daed2e8e6fe8
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-06-15 21:24:52 +0800

    fix--event

--- backend/src/handlers/auth/authorizer.mjs
+++ backend/src/handlers/auth/authorizer.mjs
@@ -10,6 +10,8 @@ const policyStoreId = process.env.POLICY_STORE_ID;
 
 export const verifiedPermissionsAuthorizerHandler = async (event) => {
   try {
+    console.log("event: ", event);
+
     const token = event.headers.authorization || event.headers.Authorization;
     if (!token) throw new Error("Missing token");
 
@@ -17,7 +19,10 @@ export const verifiedPermissionsAuthorizerHandler = async (event) => {
     const principalId = decoded["cognito:groups"].find(
       (g) => !g.endsWith("_Google")
     );
+    console.log("principalId: ", principalId);
+
     const actionId = event.headers["X-Required-Permission"];
+    console.log("actionId: ", actionId);
     const methodArn = event.methodArn;
 
     const command = new IsAuthorizedCommand({
