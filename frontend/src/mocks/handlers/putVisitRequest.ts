3f82857f4bdf4e3c3f6b1a748f54132d42b5508d
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-07-17 17:01:33 +0800

    submit visit request mock

--- frontend/src/mocks/handlers/putVisitRequest.ts
+++ frontend/src/mocks/handlers/putVisitRequest.ts
@@ -1,7 +1,7 @@
 import { RestHandler, rest } from 'msw'
 
 export const putVisitRequestHandler = (): RestHandler[] => [
-  rest.post('/visit-request', async (req, res, ctx) => {
+  rest.post('visit-request', async (req, res, ctx) => {
     await new Promise((resolve) => setTimeout(resolve, 1500))
     return res(
       ctx.status(200),
