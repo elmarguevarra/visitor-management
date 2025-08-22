379c3c2d81040bb28b26ec5d5cd58ce4909b0705
Elmar Guevarra <elmarguevarra31@gmail.com>
2025-08-23 01:10:43 +0800

    cleanup

--- frontend/src/views/ListVisitorsView.vue
+++ frontend/src/views/ListVisitorsView.vue
@@ -331,7 +331,6 @@ export default {
     }
 
     const getVisitors = async () => {
-      uiStore.isLoading = true
       try {
         const response = await getVisitorsByResidentId(
           authenticationStore.userEmail,
@@ -340,8 +339,6 @@ export default {
       } catch (error) {
         console.debug(error)
         errorMsg.value = 'Error retrieving data'
-      } finally {
-        uiStore.isLoading = false
       }
     }
 
